426c235af8d652690c6c519fb71a21cb54ce9968
ap <ap@ap>
2024-12-02 23:31:45 +0100

    autograd fix

--- config.py
+++ config.py
@@ -32,7 +32,7 @@ class PDEConfig:
     #relax
     ds = 1e2
 
-    jacobi_w = 0.3
+    jacobi_w = 0.4
 
     #uncomment to choose linear solver sparse conjuate gradient
     #linear_solver = SolverType.SPARSE_INDIRECT_BLOCK_CG
