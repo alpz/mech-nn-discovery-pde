619beba0d4e2ce3845469a2a04887ea4d0def76f
ap <ap@ap>
2025-07-12 18:15:07 +0200

    dense clean

--- solver/pde_layer_dense.py
+++ solver/pde_layer_dense.py
@@ -2,9 +2,9 @@
 import numpy as np
 import torch
 
-import cupy as cp
-import cupyx.scipy.sparse as CSP
-import cupyx.scipy.sparse.linalg as CSPLA
+#import cupy as cp
+#import cupyx.scipy.sparse as CSP
+#import cupyx.scipy.sparse.linalg as CSPLA
 
 def seed_everything(seed: int):
     import random, os
@@ -31,18 +31,8 @@ from typing import List
 import torch.nn.functional as F
 from solver.lp_pde_central_diff import PDESYSLP as PDESYSLPEPS #as ODELP_sys
 
-#add multigrid
-#from solver.qp_dual_indirect_sparse_pde import QPFunction as QPFunctionSys
-#from solver.qp_dual_sparse_multigrid_normal import QPFunction as QPFunctionSys
 import solver.qp_dual_dense_normal_kkt as MGS #import QPFunction as QPFunctionSys
-#import solver.qp_dual_sparse_multigrid_normal2 as MGS #import QPFunction as QPFunctionSys
-#import solver.qp_dual_sparse_multigrid_normal_dense as MGS #import QPFunction as QPFunctionSys
 from config import PDEConfig as config
-#import solver.cg as CG
-
-#from torch.autograd import gradcheck
-# set of KKT matrices
-#torch.autograd.detect_anomaly()
 
 
 class PDEDenseLayer(nn.Module):
@@ -78,24 +68,16 @@ class PDEDenseLayer(nn.Module):
 
         dtype = torch.float64 if self.solver_dbl else torch.float32
 
-        #self.mg_solver = MultigridSolver(bs, order, n_ind_dim, n_iv, init_index_mi_list, coord_dims, 
-        #                            n_iv_steps, solver_dbl=True, n_grid=n_grid,
-        #                            evolution=self.evolution,
-        #                            gamma=0.5, alpha=0.1, double_ret=False, 
-        #                            device=None)
         self.pde = PDESYSLPEPS(bs=bs*self.n_ind_dim, n_equations=self.n_equations, n_auxiliary=0, coord_dims=self.coord_dims, 
                                step_size=self.step_size, order=self.order, evolution=False,
                                 n_iv=self.n_iv, init_index_mi_list=init_index_mi_list, 
                                 n_iv_steps=self.n_iv_steps, dtype=dtype, device=self.device)
 
-        #self.pde = self.mg_solver.pde_list[0]
         self.n_orders = len(self.pde.var_set.mi_list)
         self.grid_size = self.pde.var_set.grid_size
         #self.step_grid_size = self.pde.step_grid_size
         self.step_grid_shape = self.pde.step_grid_shape
-        #self.iv_grid_size = self.pde.t0_grid_size
 
-        #self.qpf = MGS.QPFunction(self.pde, self.n_iv, gamma=gamma, alpha=alpha, double_ret=double_ret)
         self.qpf = MGS.QPFunction(self.pde, double_ret=double_ret)
 
     def forward(self, coeffs, rhs, iv_rhs, steps_list):
